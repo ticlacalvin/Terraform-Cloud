9f9b2e5cd2cc77e5ad307de25db342965189282e
Ubuntu <ubuntu@ip-172-31-27-58.sa-east-1.compute.internal>
2020-11-09 12:14:24 +0000

    create inst

---
+++ inst.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
