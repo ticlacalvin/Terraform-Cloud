7949c877bde82a27c03f95d26027ced2780cffda
shylajohn <70839821+shylajohn@users.noreply.github.com>
2020-11-09 18:00:17 +0530

    Update inst.tf

--- inst.tf
+++ inst.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "sa-east-1"
+  access_key=var.AWS_ACCESS_KEY
+  secret_key=var.AWS_SECRET_KEY 
 }
 resource "aws_instance" "web" {
   ami           = "ami-02dc8ad50da58fffd"
