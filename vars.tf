e41e1eb4dae11c6761611ec10736c5ef2dd24791
ticlacalvin <93556160+ticlacalvin@users.noreply.github.com>
2022-08-16 00:20:53 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,2 +1,10 @@
-variable "AWS_ACCESS_KEY"{}
-variable "AWS_SECRET_KEY"{} 
+variable "AWS_ACCESS_KEY"{
+  description = "Given name in the credential file"
+  type         = string
+  default    = AKIAUGJYG44NPD3IXNZU
+}
+variable "AWS_SECRET_ACCES_KEY"{
+  description = "Given name in the credential file"
+  type         = string
+  default    =  s4RdgwMuMdT4gdH7DSvtIXctz1mzHY53osW7JqcR
+} 
